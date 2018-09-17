ec0a31f4af971a7943059480ef1898d203fa9ca0
NayoLx <690575679@qq.com>
2018-09-17 18:14:37 +0800

    U 修改订单页面

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,14 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    voucher: []
+  },
+  /**
+   * 显示按钮
+   */
+  showSwiper() {
+    this.setData({
+      swiper: true,
+    })
+  },
+  hideSwiper() {
+    this.setData({
+      swiper: false,
+    })
   },
 
+  bindChange: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  /**
+   * 点击tab切换
+   */
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    /**
+     * 获取系统信息
+     */
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    console.log(this.data.winHeight)
   },
 
   /**
@@ -36,26 +86,6 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
 
   /**
    * 用户点击右上角分享

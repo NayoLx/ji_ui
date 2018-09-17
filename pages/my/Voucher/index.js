082286ebb1017515bbe47a1ad203c4b31afb4d4f
NayoLx <690575679@qq.com>
2018-09-17 17:43:18 +0800

    A 新增我的优惠券的页面 U 修改了地图底部button
    Signed-off-by: NayoLx <690575679@qq.com>

---
+++ pages/my/Voucher/index.js
@@ -0,0 +1,181 @@
+// pages/my/Voucher/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    voucher: []
+  },
+  /**
+   * 显示按钮
+   */
+  showSwiper() {
+    this.setData({
+      swiper: true,
+    })
+  },
+  hideSwiper() {
+    this.setData({
+      swiper: false,
+    })
+  },
+
+  bindChange: function(e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  /**
+   * 点击tab切换
+   */
+  swichNav: function(e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getVoucher()
+    var that = this;
+    /**
+     * 获取系统信息
+     */
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    console.log(this.data.winHeight)
+  },
+
+
+  getVoucher: function() {
+    var data = [{
+      "id": 0,
+      "value": 50,
+      "title": "满100减50优惠券",
+      "timeTitle": "2018.10.01-2018.10.31 有效",
+      "HowUse": "单笔维修费满100元可用",
+      "using": false,
+      "timeOut": false
+    }, {
+        "id": 1,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": false
+      }, {
+        "id": 2,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": false
+      }, {
+        "id": 3,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": false
+      }, {
+        "id": 4,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": true
+      }, {
+        "id": 5,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": true
+      },
+      {
+        "id": 6,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": true,
+        "timeOut": true
+      },
+      {
+        "id": 7,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": false,
+        "timeOut": false
+      },
+      {
+        "id": 8,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": true,
+        "timeOut": false
+      },
+      {
+        "id": 9,
+        "value": 50,
+        "title": "满100减50优惠券",
+        "timeTitle": "2018.10.01-2018.10.31 有效",
+        "HowUse": "单笔维修费满100元可用",
+        "using": true,
+        "timeOut": false
+      },]
+
+    this.setData({
+      voucher: data
+    })
+  },
+
+  getVoucherId: function(e) {
+    var a = e.currentTarget.id
+    console.log(a)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+
+})
